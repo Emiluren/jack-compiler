485eaab0d2a4c90235e4d047721644e9b30e649c
Emil Segerbäck <emilsegerback@gmail.com>
2015-10-17 14:23:22 +0200

    finished the lexer

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ mod jack_analyzer;
 use jack_analyzer::*;
 use std::env;
 use std::fs::File;
+use std::io::prelude::*;
 
 fn write_tag(tag_name: &str, content: &str) -> String {
     format!("<{0}> {1} </{0}>\n", tag_name, content)
 }
 
-fn keyword_to_str(keyword: Keyword) -> &'static str {
-    match keyword {
+fn keyword_to_str(keyword: &Keyword) -> &'static str {
+    match *keyword {
         Keyword::Class => "class",
         Keyword::Method => "method",
         Keyword::Function => "function",
@@ -33,8 +34,8 @@ fn keyword_to_str(keyword: Keyword) -> &'static str {
      }
 }
 
-fn get_tag_name(token_type: TokenType) -> &'static str {
-    match token_type {
+fn get_tag_name(token_type: &TokenType) -> &'static str {
+    match *token_type {
         TokenType::Keyword => "keyword",
         TokenType::Symbol => "symbol",
         TokenType::Identifier => "identifier",
@@ -43,10 +44,15 @@ fn get_tag_name(token_type: TokenType) -> &'static str {
     }
 }
 
-fn get_tag_data(analyzer: &JackAnalyzer, token_type: TokenType) -> String {
-    match token_type {
-        TokenType::Keyword => keyword_to_str(analyzer.key_word()).to_string(),
-        TokenType::Symbol => analyzer.symbol().to_string(),
+fn get_tag_data(analyzer: &JackAnalyzer, token_type: &TokenType) -> String {
+    match *token_type {
+        TokenType::Keyword => keyword_to_str(&analyzer.key_word().unwrap()).to_string(),
+        TokenType::Symbol => match analyzer.symbol() {
+            '<' => "&lt;".to_string(),
+            '>' => "&gt;".to_string(),
+            '&' => "&amp;".to_string(),
+            other => other.to_string(),
+        },
         TokenType::Identifier => analyzer.identifier(),
         TokenType::IntConst => analyzer.int_val().to_string(),
         TokenType::StringConst => analyzer.string_val(),
@@ -55,16 +61,26 @@ fn get_tag_data(analyzer: &JackAnalyzer, token_type: TokenType) -> String {
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    if args.len() != 1 {
+    let mut current_file: usize = 1;
+    if args.len() < 2 {
         println!("usage: jackcompiler filename");
     } else {
-        let mut analyzer = JackAnalyzer::new(&args[1]);
-        let mut outfile = File::create("output.xml").unwrap();
+        while current_file < args.len() {
+            let filename = &args[current_file];
+            println!("Compiling {}", filename);
+            let mut analyzer = JackAnalyzer::new(filename);
+            let mut outfile = File::create("output.xml").unwrap();
+            outfile.write_all(b"<tokens>\n").unwrap();
 
-        while analyzer.has_more_tokens() {
-            analyzer.advance();
-            let token_type = analyzer.token_type();
-            let tag_data = get_tag_data(&analyzer, token_type);
+            while analyzer.has_more_tokens() {
+                analyzer.advance();
+                let token_type = analyzer.token_type();
+                let tag_data = get_tag_data(&analyzer, &token_type);
+                outfile.write_all(write_tag(get_tag_name(&token_type), &tag_data).as_bytes()).unwrap();
+            }
+            outfile.write_all(b"</tokens>\n").unwrap();
+            println!("YOLO!");
+            current_file += 1;
         }
     }
 }
