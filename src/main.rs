8eba40dabf279a089678251e33285f2c46cd05cf
Emil Segerbäck <emilsegerback@gmail.com>
2015-10-18 22:52:07 +0200

    started writing compile_class

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,23 @@
 mod jack_analyzer;
 mod compilation_engine;
+mod xml_output;
+
 use jack_analyzer::*;
 use compilation_engine::*;
+use xml_output::*;
+
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn write_tag(tag_name: &str, content: &str) -> String {
-    format!("<{0}> {1} </{0}>\n", tag_name, content)
-}
-
-fn keyword_to_str(keyword: &Keyword) -> &'static str {
-    match *keyword {
-        Keyword::Class => "class",
-        Keyword::Method => "method",
-        Keyword::Function => "function",
-        Keyword::Constructor => "constructor",
-        Keyword::Int => "int",
-        Keyword::Boolean => "boolean",
-        Keyword::Char => "char",
-        Keyword::Void => "void",
-        Keyword::Var => "var",
-        Keyword::Static => "static",
-        Keyword::Field => "field",
-        Keyword::Let => "let",
-        Keyword::Do => "do",
-        Keyword::If => "if",
-        Keyword::Else => "else",
-        Keyword::While => "while",
-        Keyword::Return => "return",
-        Keyword::True => "true",
-        Keyword::False => "false",
-        Keyword::Null => "null",
-        Keyword::This => "this",
-     }
-}
-
-fn get_tag_name(token_type: &TokenType) -> &'static str {
-    match *token_type {
-        TokenType::Keyword => "keyword",
-        TokenType::Symbol => "symbol",
-        TokenType::Identifier => "identifier",
-        TokenType::IntConst => "integerConstant",
-        TokenType::StringConst => "stringConstant",
-    }
-}
-
-fn get_tag_data(analyzer: &JackAnalyzer, token_type: &TokenType) -> String {
-    match *token_type {
-        TokenType::Keyword => keyword_to_str(&analyzer.key_word().unwrap()).to_string(),
-        TokenType::Symbol => match analyzer.symbol() {
-            '<' => "&lt;".to_string(),
-            '>' => "&gt;".to_string(),
-            '&' => "&amp;".to_string(),
-            other => other.to_string(),
-        },
-        TokenType::Identifier => analyzer.identifier(),
-        TokenType::IntConst => analyzer.int_val().to_string(),
-        TokenType::StringConst => analyzer.string_val(),
-    }
-}
-
-fn write_token_file(infile: &String) {
-    let localname = infile.split('/').last().unwrap();
-    let outname = localname.split('.').next().unwrap().to_string() + ".xml";
-    println!("Compiling {} to {}", infile, outname);
+fn write_token_file(infile: &String, outname: &String) {
     let mut analyzer = JackAnalyzer::new(infile);
     let mut outfile = File::create(outname).unwrap();
     outfile.write_all(b"<tokens>\n").unwrap();
 
     while analyzer.has_more_tokens() {
         analyzer.advance();
-        let token_type = analyzer.token_type();
-        let tag_data = get_tag_data(&analyzer, &token_type);
-        outfile.write_all(write_tag(get_tag_name(&token_type), &tag_data).as_bytes()).unwrap();
+        write_token_tag(&mut analyzer, &mut outfile);
     }
     outfile.write_all(b"</tokens>\n").unwrap();
     println!("YOLO!");
@@ -87,7 +31,13 @@ fn main() {
     } else {
         while current_file < args.len() {
             let filename = &args[current_file];
-            write_token_file(filename);
+            let localname = filename.split('/').last().unwrap();
+            let outname = localname.split('.').next().unwrap().to_string() + ".xml";
+            println!("Compiling {} to {}", filename, outname);
+            //write_token_file(filename, &outname);
+            
+            let mut compiler = CompilationEngine::new(filename, &outname);
+            compiler.compile_class();
             current_file += 1;
         }
     }
