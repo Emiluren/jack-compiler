555f74a288d7a4ecaf5a84f340710f3b817b2570
Emil Segerbäck <emilsegerback@gmail.com>
2015-12-25 23:02:41 +0100

    started working on symbol table and vm writer

---
+++ src/vm_writer.rs
@@ -0,0 +1,47 @@
+pub struct VMWriter {
+    // TODO: add data
+}
+
+impl VMWriter {
+    // TODO: add constructor
+
+    pub fn write_push() {
+        // TODO: implement
+    }
+
+    pub fn write_pop() {
+        // TODO: implement
+    }
+
+    pub fn write_arithmetic() {
+        // TODO: implement
+    }
+
+    pub fn write_label() {
+        // TODO: implement
+    }
+
+    pub fn write_goto() {
+        // TODO: implement
+    }
+
+    pub fn write_if() {
+        // TODO: implement
+    }
+
+    pub fn write_call() {
+        // TODO: implement
+    }
+
+    pub fn write_function() {
+        // TODO: implement
+    }
+
+    pub fn wrute_return() {
+        // TODO: implement
+    }
+
+    pub fn close() {
+        // TODO: implement
+    }
+}
